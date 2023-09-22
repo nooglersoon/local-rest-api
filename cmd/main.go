d08739adc1eda6be0edd976b13f8f80e6e04e5b6
nooglersoon <nooglersoon@outlook.co.id>
2023-09-22 18:08:51 +0800

    remove docs

--- cmd/main.go
+++ cmd/main.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	handlers "github.com/nooglersoon/locale-rest-api/pkg/handlers/LocalFood"
 )
 
 func main() {
@@ -14,6 +15,12 @@ func main() {
 	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		json.NewEncoder(w).Encode("Hello, Locale!")
 	})
+
+	router.HandleFunc("/localfoods", handlers.GetAllLocalFoods).Methods(http.MethodGet)
+	router.HandleFunc("/localfood", handlers.AddLocalFood).Methods(http.MethodPost)
+	router.HandleFunc("/localfoods/{id}", handlers.GetLocalFoodById).Methods(http.MethodGet)
+	router.HandleFunc("/localfoods/{id}", handlers.UpdateLocalFood).Methods(http.MethodPut)
+	router.HandleFunc("/localfoods/{id}", handlers.DeleteLocalFood).Methods(http.MethodDelete)
 
 	log.Println("Locale Rest API is running!")
 	http.ListenAndServe(":3333", router)
