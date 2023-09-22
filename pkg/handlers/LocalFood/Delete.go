85d30f228341852d7d7d9e157c25d7014313f8f8
nooglersoon <nooglersoon@outlook.co.id>
2023-09-22 17:57:46 +0800

    Add delete food handler

---
+++ pkg/handlers/LocalFood/Delete.go
@@ -0,0 +1,26 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+	"github.com/nooglersoon/locale-rest-api/pkg/mocks"
+)
+
+func DeleteBook(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, _ := strconv.Atoi(vars["id"])
+
+	for index, localFood := range mocks.LocalFoods {
+		if localFood.Id == id {
+			mocks.LocalFoods = append(mocks.LocalFoods[:index], mocks.LocalFoods[index+1:]...)
+
+			w.Header().Add("Content-Type", "application/json")
+			w.WriteHeader(http.StatusOK)
+			json.NewEncoder(w).Encode("Deleted")
+			break
+		}
+	}
+}
